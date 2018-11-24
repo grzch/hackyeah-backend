5fbdc9d3b4c3c1d216fa5ac128a8cd01e31ab02b
grzch <fangitary@gmail.com>
2018-11-24 13:54:47 +0100

    Added local threshold instead of basic, changed test image

--- woarld/utils/photo_processing.py
+++ woarld/utils/photo_processing.py
@@ -1,4 +1,8 @@
+import numpy as np
 from PIL import Image
+from skimage.filters import threshold_adaptive
+
+THRESHOLD_VALUE = 200
 
 
 class ImageProcessor(object):
@@ -6,19 +10,28 @@ class ImageProcessor(object):
         self.image = image
         self.size = size
 
-    def process_and_get_image(self):
+    def get_binarized_image(self):
         self.resize()
-        self.grayscale()
-        return self.image
+        return self.grayscale_and_binarize()
 
     def resize(self):
         self.image = self.image.resize(self.size)
 
-    def grayscale(self):
-        self.image = self.image.convert("L")
+    def grayscale_and_binarize(self):
+        self.image = self.image.convert('L')
+        return self.threshold(np.asarray(self.image))
+
+    @staticmethod
+    def threshold(image):
+        block_size = 35
+        return threshold_adaptive(image, block_size, offset=10)
 
 
 if __name__ == '__main__':
     sample_img = Image.open('test.jpg')
-    processed_img = ImageProcessor(sample_img, (100, 100)).process_and_get_image()
-    processed_img.save("processed_test.jpg")
+    size = (500, 500)
+    binarized_img = ImageProcessor(sample_img, size).get_binarized_image()
+    binarized_img = binarized_img.reshape(size)
+    binarized_img = binarized_img.reshape(size).astype('uint8') * 255
+    img = Image.fromarray(binarized_img)
+    img.save("processed_test.jpg")
